40d4c5b915649817cacafb130a6eb73889e7f375
sollendy <edlannino@tutanota.com>
2023-11-03 15:40:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <HeaderComponent/>
+  <RouterView />
+  <FooterComponent/>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+    HeaderComponent,
+    FooterComponent
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+    </header>
+</template>
+  
+<script>
+export default {
+    name: 'HeaderComponent',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+  
+<style lang="scss" scoped></style>
