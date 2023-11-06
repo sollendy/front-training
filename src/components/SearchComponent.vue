e8ac4d4eb3024906c836d58f443c65f960b1620d
MartinaDR1 <derosemartina933@gmail.com>
2023-11-06 18:35:16 +0100

    add style to SearchView

---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    name: 'SearchComponent',
+}
+</script>
+<template>
+    <div class="container">
+        <div class="row g-5 my-5">
+            <div class="col-md-4">
+                <small for="where">Dove</small>
+                <input type="text" class="form-control" id="where" name="where" placeholder="Cerca una destinazione">
+            </div>
+            <div class="col-md-4">
+                <small for="rooms">Numero di stanze</small>
+                <input type="number" class="form-control" id="rooms" name="rooms"
+                    placeholder="Inserisci il numero minimo di stanze">
+            </div>
+            <div class="col-md-4">
+                <small for="beds">Numero posti letto</small>
+                <input type="number" class="form-control" id="beds" name="beds"
+                    placeholder="Inserisci il numero minimo di letti">
+            </div>
+            <div class="col-md-8">
+                <small for="service">Servizi</small>
+                <select class="form-select" aria-small="Default select example" >
+                    <option disabled selected>Seleziona i servizi che desideri</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+            <div class="col-md-4">
+                <small for="km">Raggio di ricerca</small>
+                <input type="number" class="form-control" id="km" name="km"
+                    placeholder="Cerca entro un limite di Km">
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped></style>
