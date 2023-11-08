8cb13ac33057a2385a89ab366bb6b0387a675f8c
MartinaDR1 <derosemartina933@gmail.com>
2023-11-08 18:12:40 +0100

    add multiselect

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,24 @@
 import { reactive } from "vue";
+import axios from "axios";
 
 export const store = reactive({
-  
+    API_URL_BASE: "http://127.0.0.1:8000/",
+    API_Apartments: "api/apartments",
+    services: [],
+
+    fetchServices() {
+        const url = this.API_URL_BASE + this.API_Apartments;
+        axios
+            .get(url)
+            .then(response => {
+                if (response.data.success) {
+                    this.services = response.data.services
+                } else {
+                    this.services = []
+                }
+            })
+            .catch(error => {
+                this.error = error.message
+            })
+    }
 });
